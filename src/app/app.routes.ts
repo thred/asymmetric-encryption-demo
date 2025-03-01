6f559add4934713133a6acad3ca56ac8417aa8fb
Manfred Hantschel <thred@users.noreply.github.com>
2025-03-01 21:04:51 +0100

    Initial gh-pages

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@angular/router";
-import { RsaPageComponent } from "./rsa-page/rsa-page.component";
-
-export const routes: Routes = [
-    { path: "rsa", component: RsaPageComponent },
-
-    { path: "", redirectTo: "rsa", pathMatch: "full" },
-];
--- src/app/utils/key.error-state-matcher.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl, FormGroupDirective, NgForm } from "@angular/forms";
-import { ErrorStateMatcher } from "@angular/material/core";
-import { Utils } from "./utils";
-
-export class KeyErrorStateMatcher implements ErrorStateMatcher {
-    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-        return control?.value === undefined || !Utils.isValidKey(control.value);
-    }
-}
